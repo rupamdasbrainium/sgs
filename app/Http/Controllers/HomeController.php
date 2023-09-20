961885f14142a0a8399e603bbd573494db700194
Hemanta kundu <hemanta.kundu@brainiuminfotech.com>
2023-09-20 12:00:12 +0530

    test next two api in membership page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -69,8 +69,9 @@ class HomeController extends Controller
     }
 
     public function planType($id){
-        $api = '{ "error": null, "isErrorConnString": false, "data": [ { "id": 6, "name_english": "10 passages adulte", "name_french": "10 passages adulte" }, { "id": 12, "name_english": "6 mois Adulte", "name_french": "12 mois reg" }, { "id": 18, "name_english": "3 mois Adulte", "name_french": "3 mois" }]}';
-        $data = json_decode($api);
+        // $franchisesPlanType = '{ "error": null, "isErrorConnString": false, "data": [ { "id": 6, "name_english": "10 passages adulte", "name_french": "10 passages adulte" }, { "id": 12, "name_english": "6 mois Adulte", "name_french": "12 mois reg" }, { "id": 18, "name_english": "3 mois Adulte", "name_french": "3 mois" }]}';
+        $franchisesPlanType = APICall("SubscriptionPlans/types/".$id, "get","{}");
+        $data = json_decode($franchisesPlanType);
         $html = '';
         $li = '';
         if(isset($data)){
@@ -79,156 +80,155 @@ class HomeController extends Controller
                 $li .= "<li rel='".$value->id."'>".$value->name_english."</li>";
             }
         }
-        return [$html,$li];
-        $franchisesPlanType = APICall("SubscriptionPlans/types/".$id, "get","{}");
-        return json_decode($franchisesPlanType);
+        return [$html,$li]; 
     }
 
     public function planTypeDetails($id){
-        $api = '{
-            "error": null,
-            "isErrorConnString": false,
-            "data": {
-              "id": 12,
-              "name": "12 mois reg",
-              "age_min": 0,
-              "age_max": 120,
-              "id_category": 2,
-              "id_frinchise": 3,
-              "prices_per_durations": [
-                {
-                  "duration_id": 5,
-                  "duration_unit_id": 0,
-                  "duration_unit_display": "mois",
-                  "frequency": 12,
-                  "price_initial": 9.57,
-                  "price_recurant": 39.99,
-                  "installments": [
-                    {
-                      "id": 160,
-                      "number_of_payments": 6
-                    }
-                  ]
-                }
-              ],
-              "options": [
-                {
-                  "id": 5,
-                  "name": "Nouvelle/remplacement de carte d",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 20,
-                  "is_initial": true
-                },
-                {
-                  "id": 7,
-                  "name": "12 seance d",
-                  "quantity": 1,
-                  "deliverable_quantity": 0,
-                  "price": 200,
-                  "is_initial": true
-                },
-                {
-                  "id": 8,
-                  "name": "24 séance d",
-                  "quantity": 1,
-                  "deliverable_quantity": 0,
-                  "price": 350,
-                  "is_initial": true
-                },
-                {
-                  "id": 9,
-                  "name": "Le package de 12 shake ",
-                  "quantity": 1,
-                  "deliverable_quantity": 12,
-                  "price": 12.55,
-                  "is_initial": true
-                },
-                {
-                  "id": 11,
-                  "name": "Gourde a l",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 0,
-                  "is_initial": true
-                },
-                {
-                  "id": 13,
-                  "name": "Acces au cours de zumba",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 7.5,
-                  "is_initial": true
-                },
-                {
-                  "id": 14,
-                  "name": "Accès au cours de spinning",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 100,
-                  "is_initial": true
-                },
-                {
-                  "id": 15,
-                  "name": "Accès au cours de cardioboxe",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 120,
-                  "is_initial": true
-                },
-                {
-                  "id": 16,
-                  "name": "Accès a tous les cours du centre 25$ par mois",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 25,
-                  "is_initial": false
-                },
-                {
-                  "id": 17,
-                  "name": "block 50 minute",
-                  "quantity": 1,
-                  "deliverable_quantity": 50,
-                  "price": 10,
-                  "is_initial": true
-                },
-                {
-                  "id": 18,
-                  "name": "Yoga",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 8.3,
-                  "is_initial": false
-                },
-                {
-                  "id": 19,
-                  "name": "Shake 5",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 12.5,
-                  "is_initial": true
-                },
-                {
-                  "id": 20,
-                  "name": "Assurance annulation",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 20,
-                  "is_initial": true
-                },
-                {
-                  "id": 21,
-                  "name": "tonus",
-                  "quantity": 1,
-                  "deliverable_quantity": 1,
-                  "price": 6.29,
-                  "is_initial": true
-                }
-              ],
-              "inclusions": []
-            }
-          }';
-        $data = json_decode($api);
+        // $franchisesPlanDetails = '{
+        //     "error": null,
+        //     "isErrorConnString": false,
+        //     "data": {
+        //       "id": 12,
+        //       "name": "12 mois reg",
+        //       "age_min": 0,
+        //       "age_max": 120,
+        //       "id_category": 2,
+        //       "id_frinchise": 3,
+        //       "prices_per_durations": [
+        //         {
+        //           "duration_id": 5,
+        //           "duration_unit_id": 0,
+        //           "duration_unit_display": "mois",
+        //           "frequency": 12,
+        //           "price_initial": 9.57,
+        //           "price_recurant": 39.99,
+        //           "installments": [
+        //             {
+        //               "id": 160,
+        //               "number_of_payments": 6
+        //             }
+        //           ]
+        //         }
+        //       ],
+        //       "options": [
+        //         {
+        //           "id": 5,
+        //           "name": "Nouvelle/remplacement de carte d",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 20,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 7,
+        //           "name": "12 seance d",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 0,
+        //           "price": 200,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 8,
+        //           "name": "24 séance d",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 0,
+        //           "price": 350,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 9,
+        //           "name": "Le package de 12 shake ",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 12,
+        //           "price": 12.55,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 11,
+        //           "name": "Gourde a l",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 0,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 13,
+        //           "name": "Acces au cours de zumba",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 7.5,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 14,
+        //           "name": "Accès au cours de spinning",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 100,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 15,
+        //           "name": "Accès au cours de cardioboxe",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 120,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 16,
+        //           "name": "Accès a tous les cours du centre 25$ par mois",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 25,
+        //           "is_initial": false
+        //         },
+        //         {
+        //           "id": 17,
+        //           "name": "block 50 minute",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 50,
+        //           "price": 10,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 18,
+        //           "name": "Yoga",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 8.3,
+        //           "is_initial": false
+        //         },
+        //         {
+        //           "id": 19,
+        //           "name": "Shake 5",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 12.5,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 20,
+        //           "name": "Assurance annulation",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 20,
+        //           "is_initial": true
+        //         },
+        //         {
+        //           "id": 21,
+        //           "name": "tonus",
+        //           "quantity": 1,
+        //           "deliverable_quantity": 1,
+        //           "price": 6.29,
+        //           "is_initial": true
+        //         }
+        //       ],
+        //       "inclusions": []
+        //     }
+        //   }';
+        $franchisesPlanDetails = APICall("SubscriptionPlans/type/".$id, "get","{}");
+        $data = json_decode($franchisesPlanDetails);
         $html = '';
         if(isset($data)){
             foreach($data->data->prices_per_durations as $value){
@@ -283,7 +283,5 @@ class HomeController extends Controller
             }
         }
         return $html;
-        // $franchisesPlanDetails = APICall("SubscriptionPlans/type/".$id, "get","{}");
-        // return json_decode($franchisesPlanDetails);
     }
 }
