d804d6a10ccb24ea2d7b17d72bdd532a44caa38d
RahulRoyLaravel <rahul.roy@brainiuminfotech.com>
2023-09-27 11:41:34 +0530

    subscription

--- resources/views/front/suscriptionform.blade.php
+++ resources/views/front/suscriptionform.blade.php
@@ -97,7 +97,7 @@
 												@if(isset($data['provinces']))
 												@foreach ($data['provinces'] as $item)
 													
-													<option value="{{ $item->id }}" {{ $loop->index==1? "selected":""}} >{{ $item->display_english }}</option>
+													<option value="{{ $item->id }}" {{ $item ->id == old("address_province_id") ? "selected":""}} >{{ $item->display_english }}</option>
 												@endforeach
 												@endif
 												{{-- <option value="AB" >AB</option>
@@ -151,8 +151,8 @@
 												<i class="far fa-chevron-down"></i>
 											</div>
 											<select class="select_opt" name="is_male" >
-												<option value="1" selected >Male</option>
-												<option value="0">Female</option>
+												<option value="1" {{ old('is_male') == 1 ? "selected" : "" }} >Male</option>
+												<option value="0" {{ old('is_male') == 0 ? "selected" : "" }}>Female</option>
 											</select>
 										</div>
 									</div>
@@ -296,7 +296,7 @@
 												<option value="" selected >Please choose...</option>
 												@if(isset($data['opts_references']) && isset($data['opts_references']->data))
 												@foreach ($data['opts_references']->data as $item)
-													<option value="{{ $item->id }}" selected >{{ $item->display }}</option>
+													<option value="{{ $item->id }}" {{ $item ->id == old("reference_id") ? "selected":""}} >{{ $item->display }}</option>
 													
 												@endforeach
 												@endif
