a7b4293812cc6dceecddb476ce9cb4fd601a2cdf
Ayan Dey <ayandey.brainium@gmail.com>
2023-10-04 11:18:20 +0530

    login language change

---
+++ resources/lang/fr/login.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Login' => 'Se connecter',
+    'Input_your_details' => 'Saisissez vos coordonnées et votre mot de passe pour commencer',
+    'Username' => 'Nom d\'utilisateur',
+    'Password' => 'Mot de passe',
+    'captcha' => 'Entrez le texte dans l\'image',
+    'validcaptcha' => 'Entrez un captcha valide',
+    'Remember_me' => 'Souviens-toi de moi',
+    'Forgot_Password' => 'Mot de passe oublié?',
+];
