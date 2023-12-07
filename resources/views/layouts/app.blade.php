e181fe051b448733dfbef99c56194ff9e02ae345
Ayan Dey <ayandey.brainium@gmail.com>
2023-12-07 18:01:38 +0530

    cnange

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,8 @@
     <div class="font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
+    <script src="{{ asset('public/js/jquery.min.js') }}"></script>
     <script src="{{ asset('public/admin/js/owl.carousel.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
