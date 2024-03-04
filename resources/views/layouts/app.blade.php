5a3698dc4c895e9adfc1b2552ede91918c191a47
RahulRoyLaravel <rahul.roy@brainiuminfotech.com>
2024-03-04 15:05:24 +0530

    name change of site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') {{ __('app.name') }}</title>
+    <title>@yield('title') {{ config('app.name', 'Softsgs') }}</title>
     <style>
         :root {
             @yield('style')
